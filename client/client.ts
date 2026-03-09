6efd8f8561257bf35145e384b35af49b16b152e1
Rinleon <163319710+Rinleon@users.noreply.github.com>
2026-03-09 01:53:26 -0600

    listo para hacer pruebas

--- client/client.ts
+++ client/client.ts
@@ -8,93 +8,96 @@ const provider = anchor.AnchorProvider.env();
 anchor.setProvider(provider);
 
 const program = anchor.workspace.Juegos as Program<any>;
-
 const wallet = provider.wallet;
 
 const [jjuegoPda] = PublicKey.findProgramAddressSync(
-  [
-    Buffer.from("jjuego"),
-    wallet.publicKey.toBuffer(),
-  ],
-  program.programId
+[
+Buffer.from("jjuego"),
+wallet.publicKey.toBuffer(),
+],
+program.programId
 );
 
 console.log("PDA:", jjuegoPda.toString());
 
-/* -----------------------------
-CREAR CUENTA
-------------------------------*/
+//////////////////////////////////////////////////
+// PRUEBAS RAPIDAS
+//////////////////////////////////////////////////
+
+// await crearCuenta();
+
+await agregarJuego("R.E.P.O",170,"xbox","Horror");
+await agregarJuego("Metal slug ",170,"xbox","ACCION");
+await agregarJuego("Tomb raider",170,"xbox","ACCION");
+await cambiarDisponibilidad("Devil may cry 4");
+await eliminarJuego("metal slug");
+await verJuegos();
+
+async function crearCuenta(){
 
 await program.methods
-  .correrJuego("mi_lista")
-  .accounts({
-    cuenta: wallet.publicKey,
-    jjuego: jjuegoPda,
-    systemProgram: anchor.web3.SystemProgram.programId,
-  })
-  .rpc();
+.correrJuego("mi_lista")
+.accounts({
+cuenta: wallet.publicKey,
+jjuego: jjuegoPda,
+systemProgram: anchor.web3.SystemProgram.programId
+})
+.rpc();
 
 console.log("Cuenta creada");
 
+}
 
-/* -----------------------------
-AGREGAR JUEGO
-------------------------------*/
-
-await program.methods
-  .agregarJuego(
-    "Resident Evil 4",
-    200,
-    "PC",
-    "Survival Horror"
-  )
-  .accounts({
-    cuenta: wallet.publicKey,
-    jjuego: jjuegoPda,
-  })
-  .rpc();
-
-console.log("Juego agregado");
-
-
-/* -----------------------------
-VER JUEGOS
-------------------------------*/
-
-const cuenta = await program.account.tjuego.fetch(jjuegoPda);
-
-console.log("Juegos guardados:");
-console.log(cuenta.juegos);
-
-
-/* -----------------------------
-CAMBIAR DISPONIBILIDAD
-------------------------------*/
+async function agregarJuego(nombre:string,precio:number,plataforma:string,genero:string){
 
 await program.methods
-  .cambiosJuego("Resident Evil 4")
-  .accounts({
-    cuenta: wallet.publicKey,
-    jjuego: jjuegoPda,
-  })
-  .rpc();
+.agregarJuego(nombre,precio,plataforma,genero)
+.accounts({
+cuenta: wallet.publicKey,
+jjuego: jjuegoPda
+})
+.rpc();
+
+console.log("Juego agregado:",nombre);
+
+}
+
+async function cambiarDisponibilidad(nombre:string){
+
+await program.methods
+.cambiosJuego(nombre)
+.accounts({
+cuenta: wallet.publicKey,
+jjuego: jjuegoPda
+})
+.rpc();
 
 console.log("Disponibilidad cambiada");
 
+}
 
-/* -----------------------------
-ELIMINAR JUEGO
-------------------------------*/
+async function eliminarJuego(nombre:string){
 
 await program.methods
-  .eliminarJuego("Resident Evil 4")
-  .accounts({
-    cuenta: wallet.publicKey,
-    jjuego: jjuegoPda,
-  })
-  .rpc();
+.eliminarJuego(nombre)
+.accounts({
+cuenta: wallet.publicKey,
+jjuego: jjuegoPda
+})
+.rpc();
 
 console.log("Juego eliminado");
+
+}
+
+async function verJuegos(){
+
+const cuenta = await program.account.tjuego.fetch(jjuegoPda);
+
+console.log("Lista de juegos:");
+console.log(cuenta.juegos);
+
+}
 
 }
 
