5450e5fa20861819ab76f09cf690f23400086cd9
Rinleon <163319710+Rinleon@users.noreply.github.com>
2026-03-09 01:02:34 -0600

    nueva version

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,150 @@
 use anchor_lang::prelude::*;
 
-declare_id!(**);
+declare_id!("2sm1vNABdUrSB8tWC8FZksiC18S85mXUpA7tXtYcVNzK");
+
 #[program]
-pub mod juegos{
+pub mod juegos {
     use super::*;
 
-    pub fn correr_juego() ->  Result<()>{
+    pub fn correr_juego(context: Context<NuevoTjuego>, nombre: String) -> Result<()> {
 
+        let cuenta_id = context.accounts.cuenta.key();
+        let juegos: Vec<Juego> = Vec::new();
+
+        context.accounts.jjuego.set_inner(Tjuego{
+            cuenta: cuenta_id,
+            nombre,
+            juegos,
+        });
+
+        Ok(())
     }
 
+    pub fn agregar_juego(context: Context<Nuevojuego>,nombre: String,precio: u16,plataforma: String,genero: String) -> Result<()> {
+
+        let juego: Juego = Juego{
+            nombre,
+            precio,
+            plataforma,
+            genero,
+            disponible: true,
+        };
+
+        context.accounts.jjuego.juegos.push(juego);
+
+        Ok(())
+    }
+
+    pub fn ver_juego(context: Context<Nuevojuego>) -> Result<()> {
+
+        let juegos = &context.accounts.jjuego.juegos;
+
+        msg!("Estos son los juegos: {:#?}", juegos);
+
+        Ok(())
+    }
+    
+    pub fn eliminar_juego(context: Context<Nuevojuego>, nombre: String) -> Result<()> {
+
+    let juegos = &mut context.accounts.jjuego.juegos;
+
+    for i in 0..juegos.len() {
+        if juegos[i].nombre == nombre {
+
+            juegos.remove(i);
+
+            msg!("Este juego {} está game over", nombre);
+
+            return Ok(());
+        }
+    }
+
+    Err(Errores::JuegoNoExiste.into())
 }
-#[Account]
-#[deriveve(InitSpace)]
+
+
+   pub fn cambios_juego(context: Context<Nuevojuego>, nombre: String) -> Result<()> {
+    let juegos = &mut context.accounts.jjuego.juegos;
+    for i in 0..juegos.len() {
+
+        if juegos[i].nombre == nombre {
+
+            let cambios = juegos[i].disponible;
+            let nuevo_cambio = !cambios;
+            juegos[i].disponible = nuevo_cambio;
+            msg!("El juego {} fue modificado. Disponible: {}", nombre, nuevo_cambio);
+
+            return Ok(());
+        }
+    }
+
+    Err(Errores::JuegoNoExiste.into())
+}
+}
+
+
+#[error_code]
+pub enum Errores{
+    #[msg("ERROR, QUIEN ERES TU!?????")]
+    NoEresLacuenta,
+
+    #[msg("Error ese Juego no existe :,c")]
+    JuegoNoExiste,
+}
+
+#[account]
+#[derive(InitSpace)]
 pub struct Tjuego{
-    cuenta: Pubkey,
-    #{max_len(60)}
-    nombre: String,
+    pub cuenta: Pubkey,
 
-    juegos: Vec<Juego>,
+    #[max_len(10)]
+    pub nombre: String,
+
+    #[max_len(10)]
+    pub juegos: Vec<Juego>,
 }
 
-#[derive(AnchorDeserialize,AnchorDeserialize,Clone,InitSpace,PartialEq,Debug)]
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace, PartialEq, Debug)]
 pub struct Juego{
-    #{amx_len(60)}
-    nombre:String,
-    precio:u16,
-    #{amx_len(60)}
-    plataforma: String,
-    #{amx_len(30)}
-    genero: String,
 
-    disoinible: bool,
+    #[max_len(60)]
+    pub nombre:String,
+
+    pub precio:u16,
+
+    #[max_len(60)]
+    pub plataforma:String,
+
+    #[max_len(30)]
+    pub genero: String,
+
+    pub disponible: bool,
 }
 
-///Contexto
+/// Contexto
 
 #[derive(Accounts)]
-pub struct NuevoTjuego{
-    #{account(mut)}
-    pub cuenta:Signer<'info>,
-    pub jjuego:AAccount<'info,Tjuego>,
-    pub system_program:Program<'info,System>,
-}
+pub struct NuevoTjuego<'info>{
 
-pub struct Nuevojuego{
-     #[account(
+    #[account(mut)]
+    pub cuenta: Signer<'info>,
+
+    #[account(
         init,
         payer = cuenta,
-        space = Tjuego::INTt_SPACE + 8,
-        seeds = [b"jjuego",cuenta.key().as_ref()]
+        space = 8 + Tjuego::INIT_SPACE,
+        seeds = [b"jjuego", cuenta.key().as_ref()],
         bump
      )]
-    pub cuenta:Signer<'info>,
+    pub jjuego: Account<'info,Tjuego>,
 
+    pub system_program: Program<'info,System>,
+}
+
+#[derive(Accounts)]
+pub struct Nuevojuego<'info>{
+
+    pub cuenta:Signer<'info>,
 
     #[account(mut)]
     pub jjuego:Account<'info, Tjuego>,
